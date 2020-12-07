5762ab04aaaad81f3769ae7c417d85de58b0dbed
Henry <new@Henrys-macbook-pro.local>
2020-12-07 11:59:09 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Hero from './components/Hero'
+import Header from './components/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className="logo-image">
+
+            </div>
+
+            <ul className="nav-links">
+                <li>Home</li>
+                <li>about</li>
+                <li>services</li>
+
+            </ul>
+            
+        </div>
+    )
+}
+
+export default Header
